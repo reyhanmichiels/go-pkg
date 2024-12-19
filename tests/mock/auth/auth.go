d32922057c04d98371094875505dd00dff5c38ca
reyhanmichiels <michiels.reyhan@gmail.com>
2024-12-19 10:48:00 +0700

    update mock

--- tests/mock/auth/auth.go
+++ tests/mock/auth/auth.go
@@ -115,12 +115,11 @@ func (mr *MockInterfaceMockRecorder) ValidateAccessToken(token any) *gomock.Call
 }
 
 // ValidateRefreshToken mocks base method.
-func (m *MockInterface) ValidateRefreshToken(token string) (int64, error) {
+func (m *MockInterface) ValidateRefreshToken(token string) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ValidateRefreshToken", token)
-	ret0, _ := ret[0].(int64)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
+	ret0, _ := ret[0].(error)
+	return ret0
 }
 
 // ValidateRefreshToken indicates an expected call of ValidateRefreshToken.
